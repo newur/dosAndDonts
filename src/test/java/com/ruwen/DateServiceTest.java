0676046d1f79a339392b9eee066595394ae059f5
U-GER\ruwen.reddig <ruwen.reddig@DEW23167.ger.win.int.kn>
2017-05-11 03:04:03 +0200

    added comments and proper method names

--- src/test/java/com/ruwen/DateServiceTest.java
+++ src/test/java/com/ruwen/DateServiceTest.java
@@ -1,11 +1,11 @@
 package com.ruwen;
 
-import com.ruwen.data.Booking;
-import com.ruwen.service.DateService;
-import com.ruwen.service.FooService;
-import org.assertj.core.api.Assertions;
-import org.junit.Assert;
-import org.junit.Test;
+import static java.util.stream.Collectors.toList;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import java.time.LocalDate;
 import java.util.Arrays;
@@ -13,53 +13,106 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 
-import static org.mockito.Mockito.*;
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.exceptions.misusing.InvalidUseOfMatchersException;
+
+import com.ruwen.data.Booking;
+import com.ruwen.service.DateService;
+import com.ruwen.service.FooService;
 
 /**
  * Created by ruwen on 24.03.17.
  */
 public class DateServiceTest {
 
-    private FooService fooService;
+    private FooService fooService = mock(FooService.class);
 
-    private Booking booking = Booking.builder().trackingNumber("1234").build();
+    private static final String TRACKING_NUMBER = "1234";
+
+    private Booking booking;
 
     @Test
-    public void testFooService() {
-        fooService = mock(FooService.class);
-        DateService dateService = new DateService();
+    public void mockPOJOs() {
+        // Two lines to mock a getter...
+        booking = mock(Booking.class);
+        when(this.booking.getTrackingNumber()).thenReturn(TRACKING_NUMBER);
 
-        LocalDate etaPlus2Day = dateService.findEtaPlus2Day(booking);
+        LocalDate etaPlus2Day = new DateService().findEtaPlus2Day(this.booking);
 
-        Assertions.assertThat(etaPlus2Day.toString()).contains("2017");
+        assertThat(etaPlus2Day.toString()).contains("2017");
     }
 
     @Test
-    public void someStreamTest() {
+    public void mockPOJOs_alternative() {
+        // Use a builder if available and consider moving it to a field level
+        booking = Booking.builder().trackingNumber("1234").build();
+
+        LocalDate etaPlus2Day = new DateService().findEtaPlus2Day(this.booking);
+
+        assertThat(etaPlus2Day.toString()).contains("2017");
+    }
+
+    @Test
+    public void checkSomethingIsNotPresentInStream() {
+        String[] names = {"Torben", "Anthony", "Sven", "Uli"};
+
+        // '!' can easily be overlooked in a multi-liner
+        boolean isSvenAbsent = !Arrays.stream(names)
+                // will throw NullPointerException if list contains null values +
+                // .filter .findFirst .isPresent is a lot of boilerplate code
+                .filter(s -> s.equals("Sven"))
+                .findFirst()
+                .isPresent();
+
+        Assert.assertFalse(isSvenAbsent);
+    }
+
+    @Test
+    public void checkSomethingIsNotPresentInStream_alternative() {
         String[] names = {"Torben", null, "Anthony", "Sven", "Uli"};
 
-        // check none matches the given value
-        boolean svenNotPresent = Arrays.stream(names)
+        boolean isSvenAbsent = Arrays.stream(names)
+                // check none matches the given value + null safe
                 .noneMatch("Sven"::equals);
 
-        Assert.assertFalse(svenNotPresent);
+        Assert.assertFalse(isSvenAbsent);
     }
 
+    // Could not find the anti pattern anymore, so here directly the "right" way
     @Test
-    public void streamBirthdays() {
-        Set<List<String>> birthdays = new DateService().findMixedBeerBoxes();
+    public void collectionContainsCollection_alternative() {
+        Set<List<String>> boxes = new DateService().findMixedBeerBoxes();
 
-        birthdays.stream()
+        List<String> beers = boxes.stream()
+                // flatten the nested collection to get one with all sub elements
                 .flatMap(Collection::stream)
-                .anyMatch(name -> name.equals("Astra"));
+                .collect(toList());
+
+        beers.stream().forEach(System.out::println);
     }
 
     @Test
-    public void testServiceWith2Parameters() {
+    public void mockShouldReturnForOneSpecificAndOneArbitraryInput() {
+        try {
+            // this will not work. Throws InvalidUseOfMatchersException
+            when(fooService.findStringBy2Parameters(any(), "hello")).thenReturn("bang");
+        } catch (InvalidUseOfMatchersException e) {
+            // stack trace explains how to do it. Find an example below
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void mockShouldReturnForOneSpecificAndOneArbitraryInput_alternative() {
+        // use matchers on both arguments
         when(fooService.findStringBy2Parameters(any(), eq("hello"))).thenReturn("bang");
 
         String stringBy2Parameters = fooService.findStringBy2Parameters("world", "hello");
+        assertThat(stringBy2Parameters).isEqualTo("bang");
 
-        Assertions.assertThat(stringBy2Parameters).isEqualTo("bang");
+        stringBy2Parameters = null;
+        stringBy2Parameters = fooService.findStringBy2Parameters("world", "wrongInput");
+        assertThat(stringBy2Parameters).isNull();
     }
 }
--- src/test/java/com/ruwen/OptionalTest.java
+++ src/test/java/com/ruwen/OptionalTest.java
@@ -1,13 +1,14 @@
 package com.ruwen;
 
+import java.util.Optional;
+
+import org.junit.Test;
+
 import com.ruwen.data.Booking;
 import com.ruwen.data.enums.ModeOfTransport;
 import com.ruwen.service.ExpensiveService;
 import com.ruwen.service.FooService;
 import com.ruwen.service.OptionalService;
-import org.junit.Test;
-
-import java.util.Optional;
 
 public class OptionalTest {
 
@@ -15,42 +16,48 @@ public class OptionalTest {
     private ExpensiveService expensiveService = new ExpensiveService();
 
     @Test
-    public void optionalTest() {
-        ModeOfTransport modeOfTransport = null;
+    public void explicitOptionalCheck() {
+        ModeOfTransport modeOfTransport;
 
         Optional<Booking> optional = optionalService.findBooking();
 
+        // check Optional explicitly with .isPresent and then .get() it
         if (optional.isPresent()) {
             modeOfTransport = optional.get().getModeOfTransport();
         } else {
             modeOfTransport = ModeOfTransport.AIR;
         }
-
-
-//        return modeOfTransport;
+        System.out.println(modeOfTransport);
     }
 
     @Test
-    public void moreOptional() {
-        String bookingString = null;
+    public void explicitOptionalCheck_alternative() {
 
-        Optional<Booking> optional = optionalService.findBooking();
+        // avoids explicit check and intermediate variable
+        optionalService.findBooking()
+                .map(Booking::getModeOfTransport)
+                .ifPresent(System.out::println);
+    }
 
-        if (optional.isPresent()) {
-            bookingString = optional
-                    .map(Booking::toString)
-                    .orElse(expensiveService.callWebservice().toString());
-        }
+    @Test
+    public void expensiveCallsAsOptionalDefault() {
+        String bookingString = optionalService.findBooking()
+                .map(Booking::toString)
+                // operation in .orElse will be executed !!always!!
+                // do not make any expensive calls here
+                .orElse(expensiveService.callWebservice().toString());
 
         System.out.println(bookingString);
     }
 
     @Test
-    public void betterOptional() {
-        optionalService.findBooking()
+    public void expensiveCallsAsOptionalDefault_alternative() {
+        String bookingString = optionalService.findBooking()
                 .map(Booking::toString)
-
+                // .orElseGet will only be executed if Optional was empty
                 .orElseGet(() -> expensiveService.callWebservice().toString());
+
+        System.out.println(bookingString);
     }
 
     @Test
@@ -60,21 +67,33 @@ public class OptionalTest {
         FooService fooService = new FooService();
         Booking booking = fooService.findBooking();
 
+        // Uahhhh....
         if (booking != null) {
             result = Optional.of(booking);
         } else {
             result = Optional.empty();
         }
+
+        System.out.println(result);
     }
 
     @Test
-    public void slightlyLessWeirdOptionalCreation() {
-        Optional<Booking> result;
-
+    public void weirdOptionalCreation_partTwo() {
         FooService fooService = new FooService();
         Booking booking = fooService.findBooking();
 
-        result = booking != null ? Optional.of(booking) : Optional.empty();
+        // "It's a one liner...."
+        Optional<Booking> result = booking != null ? Optional.of(booking) : Optional.empty();
+        System.out.println(result);
+    }
+
+    @Test
+    public void weirdOptionalCreation_alternative() {
+        FooService fooService = new FooService();
+
+        Optional<Booking> booking = Optional.ofNullable(fooService.findBooking());
+
+        System.out.println(booking);
     }
 
 }
